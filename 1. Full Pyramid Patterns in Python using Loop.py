f9ef079b9f5183198cc6746865e7dc3c16c99056
ShivajiRam108-github <ramudus684@gmail.com>
2025-02-05 22:28:42 +0530

    P-1

--- 1. Full Pyramid Patterns in Python using Loop.py
+++ 1. Full Pyramid Patterns in Python using Loop.py
@@ -9,7 +9,12 @@ for i in range(num-2, 0, -2):
 
 
 '''simple ways '''
-# num = int(input("Enter rows: "))
-# print("\num".join([(" " * ((num - i) // 2) + "*" * i) for i in range(1, num+1, 2)] +
-#                 [(" " * ((num - i) // 2) + "*" * i) for i in range(num-2, 0, -2)]))
+num = int(input("Enter rows: "))
+print("\num".join([(" " * ((num - i) // 2) + "*" * i) for i in range(1, num+1, 2)] +
+                [(" " * ((num - i) // 2) + "*" * i) for i in range(num-2, 0, -2)]))
 
+#  single line code 
+num = int(input("Enter No of rows:"))
+for i in range(1,2 *num):
+    res = (num -i) * " " + "* " * i if i <=num else (i - num) * " " + "* " * (2 *num -i)
+    print(res)
