bb278910da4c74ed21c800a7477ca78a6ff43789
ShivajiRam108-github <ramudus684@gmail.com>
2025-02-11 23:03:25 +0530

    P-7

---
+++ 7.Number Pyramid Pattern.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a rows: "))
+for i in range(1, num+ 1):
+    print(" " * (num - i) + " ".join(str(j) for j in range(1, i + 1)))
+
+
+# # Simple Pyramid Pattern Using Stars
+num = int(input("Enter a rows:  "))
+for i in range(1,num+1):
+    print(" "*(num-i)+ "* " *i)
+
+#  reversed Pyramid Pattern Using Stars
+num = int(input("Enter a rows: "))
+for i in range(num, 0, -1):
+    print(" "* (num-i)+"* "*i)
